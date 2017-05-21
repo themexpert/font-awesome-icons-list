b5c12911eb407b0287bcb45cc0192901accd52e2
Nadim Tuhin <nadimtuhin@gmail.com>
2017-05-22 01:38:42 +0600

    updated icons.yml src to upstream repo

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Yaml\Yaml;
 
-$url = 'https://raw.githubusercontent.com/nadimtuhin/Font-Awesome/master/src/icons.yml';
+$url = 'https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/src/icons.yml';
 
 $contents = Yaml::parse(file_get_contents($url));
 $fonts = array_map(function($font) {
